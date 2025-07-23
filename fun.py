7d6cb6fc531a9db84491f8555823ed1fe86e170c
Sanjay b <sanjaysrivastavab@gmail.com>
2025-07-23 16:39:27 -0700

    py complete

---
+++ fun.py
@@ -0,0 +1,13 @@
+# def greet(name):
+#     print(f"hello {name}")
+# greet('jay')
+
+# def sum(n1,n2):
+#     return n1+n2
+# print(sum(1,2))
+
+
+def add_num(*args):
+    return sum(args)
+print(add_num(1,2,3,4))
+
