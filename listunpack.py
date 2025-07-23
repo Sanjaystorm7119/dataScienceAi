3ccb60105cfdbe35da27925bfbbea58f59b12388
Sanjay b <sanjaysrivastavab@gmail.com>
2025-07-23 12:54:59 -0700

    py part2

--- listunpack.py
+++ listunpack.py
@@ -1,7 +1,11 @@
-# a,b,c= [1,2,3]
-# print(a,b)
+a,b,c= [1,2,3]
+print(a,b)
 
-# tuple1 = (1,)
-# print(type(tuple1))
+tuple1 = (1,)
+print(type(tuple1))
+
+my_list=[1,2,3,4,5,5,5]
+print(list(set(my_list)))
 
 
+print('a'<'A')
